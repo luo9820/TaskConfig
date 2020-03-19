4704cd9ae97811d7abcbc35f20be8e54956cd332
luo9820 <58935739+luo9820@users.noreply.github.com>
2020-03-19 18:09:47 +0800

    Update weather_dark.js

--- task/weather_dark.js
+++ task/weather_dark.js
@@ -3,14 +3,14 @@
 https://github.com/sazs34/TaskConfig#%E5%A4%A9%E6%B0%94
  */
 let config = {
-    darksky_api: "填这里", //从https://darksky.net/dev/ 上申请key填入即可
-    aqicn_api: "填这里", //从http://aqicn.org/data-platform/token/#/ 上申请key填入即可
+    darksky_api: "3bfc67a280579f9ed3f023c35416e585BBC", //从https://darksky.net/dev/ 上申请key填入即可
+    aqicn_api: "b48f4e1294cc0327f02029ea5adcce8561ce66f7", //从http://aqicn.org/data-platform/token/#/ 上申请key填入即可
     huweather_apiKey: "faead3de5f42420098c8132b3924cd09", //和风天气APIkey,可自行前往 https://dev.heweather.com/ 进行获取(注意key类型选WebApi)
-    lat_lon: "填这里", //请填写经纬度,直接从谷歌地图中获取即可
+    lat_lon: "(30.8278027, 114.7968755)", //请填写经纬度,直接从谷歌地图中获取即可
     lang: "zh", //语言,请不要修改
     show: {
         log: 0, //调试日志,0为不开启,1为开启,2为开启精简日志
-        icon: true, //是否显示图标信息,不显示会比较精简
+        icon: true, //是否显示图标信息,不显示会比较 精简
         aqi: true, //空气质量以及风速显示,false则不显示
         uv: true, //紫外线显示,false则不显示
         apparent: true, //体感温度显示,false则不显示
@@ -19,16 +19,16 @@ let config = {
             cw: false, //洗车指数,
             drsg: true, //穿衣指数,
             flu: false, //感冒指数,
-            sport: false, //运动指数,
+            sport: true, //运动指数,
             trav: false, //旅游指数,
             uv: false, //紫外线指数,
-            air: false, //空气污染扩散条件指数,
+            air: true, //空气污染扩散条件指数,
             ac: false, //空调开启指数,
             ag: false, //过敏指数,
             gl: false, //太阳镜指数,
             mu: false, //化妆指数,
             airc: false, //晾晒指数,
-            ptfc: false, //交通指数,
+            ptfc: true, //交通指数,
             fsh: false, //钓鱼指数,
             spi: false, //防晒指数
         }
